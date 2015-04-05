626a9d164ee065457c475d9a6c13ae8e5f69358c
jarvisoa <jarvisoa@rose-hulman.edu>
2015-04-05 01:02:44 -0400

    lab finished

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -50,5 +50,51 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+        [TestMethod()]
+        public void TestThatCarDoesGetLocationFromDatabase()
+        {
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            String location = "Seattle";
+            Expect.Call(mockDB.getCarLocation(2)).Return(location);
+            mocks.ReplayAll();
+            Car target = new Car(10);
+
+
+            target.Database = mockDB;
+            String result;
+            result = target.getCarLocation(2);
+            Assert.AreEqual(location, result);
+            mocks.VerifyAll();
+
+        }
+
+
+        [TestMethod()]
+        public void TestThatCarDoesGetMileageFromDatabase()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            int expectedMileage = 200;
+
+            Expect.Call(mockDatabase.Miles).PropertyBehavior();
+
+            mocks.ReplayAll();
+            mockDatabase.Miles = expectedMileage;
+            var target = new Car(10);
+            target.Database = mockDatabase;
+
+            int mileage = target.Mileage;
+            Assert.AreEqual(expectedMileage, mileage );
+
+            mocks.VerifyAll();
+
+        }
+
+        [TestMethod]
+        public void TestThatCarHasCorrectBasePriceForTenDaysUsingObjectMother()
+        {
+            var target = ObjectMother.BMW();
+            Assert.AreEqual(80, target.getBasePrice());
+        }
 	}
 }
